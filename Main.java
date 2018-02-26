63d90c2a1a5604a01cc45c2b7e5b1605295490c5
Lozzad <36076143+Lozzad@users.noreply.github.com>
2018-02-26 13:47:43 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+class prog
+{
+    public static void main (String args[]) //remember to capitalise 'String'
+    {
+        System.out.println("Hello World"); //remember to capitalise 'System'
+        //test comment
+    }
+}
---
+++ prog.java
@@ -0,0 +1,8 @@
+class prog
+{
+    public static void prog (String args[]) //remember to capitalise 'String'
+    {
+        System.out.println("Hello World"); //remember to capitalise 'System'
+        //test comment
+    }
+}
